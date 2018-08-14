2bbf0645ac052b89a2f331f4d463f74463073e92
Allen Hai <allenhai03@gmail.com>
2018-08-14 11:44:37 -0500

    add isNaN check for numbers, null check for objects, and an isUndefined check

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
 }
 
 function isANumber (x) {
-  if (typeof x === 'number') {
+  if (typeof x === 'number' && !isNan(x)) {
     return true
   }
   return false
@@ -26,7 +26,7 @@ function isAnArray (x) {
 }
 
 function isAnObject (x) {
-  if (!isAnArray(x) && typeof x === 'object') {
+  if (!isAnArray(x) && typeof x === 'object' && x !== null) {
     return true
   }
   return false
@@ -41,6 +41,13 @@ function isAFunction (x) {
 
 function isBoolean (x) {
   if (typeof x === 'boolean') {
+    return true
+  }
+  return false
+}
+
+function isUndefined (x) {
+  if (typeof x === 'undefined') {
     return true
   }
   return false
